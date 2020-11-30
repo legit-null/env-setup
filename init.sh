bb9ae03c757e8361e9a4c8d24142341cd803752e
legit-null <yud.von@gmail.com>
2020-11-30 10:55:54 +0800

    Fix bug in init.sh

--- init.sh
+++ init.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/bash
 
 # ---------- general ----------
-mkdir -p $USER/bin
-mkdir -p $USER/tmp
-mkdir -p $USER/ws
+mkdir -p $HOME/bin
+mkdir -p $HOME/tmp
+mkdir -p $HOME/ws
 
 # ---------- nvim ----------
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage \
-  -P $USER/bin/
-chmod +x $USER/bin/nvim.appimage
+  -P $HOME/bin/
+chmod +x $HOME/bin/nvim.appimage
 
-mkdir -p $USER/.vim/
-mkdir -p $USER/.config/nvim
+mkdir -p $HOME/.vim/
+mkdir -p $HOME/.config/nvim
 
-cp nvim/.vimrc $USER/.vim/
-cp nvim/init.vim $USER/.config/nvim/
+cp nvim/.vimrc $HOME/.vim/
+cp nvim/init.vim $HOME/.config/nvim/
 
 # ---------- zsh ----------
 
