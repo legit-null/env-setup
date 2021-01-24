08283e065d1816912bdeeb372d6c4ffe5eb2908c
legitnull <yud.von@gmail.com>
2021-01-24 20:34:22 +0800

    Add afterparty.sh

---
+++ afterparty.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# ---------- nvim ----------
+# make sure conda is setup
+pip install neovim
+pip install clang-format
--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # ---------- general ----------
 mkdir -p $HOME/bin
@@ -6,8 +6,14 @@ mkdir -p $HOME/tmp
 mkdir -p $HOME/ws
 
 cp misc/find.sh $HOME/ws/
+echo 'set -o vi' >> $HOME/.bashrc
 echo 'alias fd="sh ~/ws/find.sh"' >> $HOME/.bashrc
 echo 'alias fd="sh ~/ws/find.sh"' >> $HOME/.zshrc
+
+# ---------- conda ----------
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+  -O $HOME/tmp/
+sh $HOME/tmp/Miniconda3-latest-Linux-x86_64.sh
 
 # ---------- nvim ----------
 echo "begin nvim setup..."
@@ -22,6 +28,13 @@ cp nvim/vimrc $HOME/.vim/
 cp nvim/init.vim $HOME/.config/nvim/
 
 ln -s $HOME/.vim/vimrc $HOME/.vimrc
+
+echo 'alias n="~/bin/nvim.appimage"' >> $HOME/.bashrc
+echo 'alias vim="~/bin/nvim.appimage"' >> $HOME/.bashrc
+echo 'alias vi="~/bin/nvim.appimage"' >> $HOME/.bashrc
+echo 'alias n="~/bin/nvim.appimage"' >> $HOME/.zshrc
+echo 'alias vim="~/bin/nvim.appimage"' >> $HOME/.zshrc
+echo 'alias vi="~/bin/nvim.appimage"' >> $HOME/.zshrc
 
 echo "done nvim setup."
 
